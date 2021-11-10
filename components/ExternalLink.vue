69c5e0a73103a52c89a98526748545b2dde4d129
agfebrian <febriananggi9@gmail.com>
2021-11-10 11:08:25 +0700

    :sparkles: sidebar content

---
+++ components/ExternalLink.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="d-flex flex-column fixed">
+    <nuxt-link v-for="(item, index) in contentTitle" :key="index" :to="item.path">
+      <p
+        class="my-1 text-subtitle-cust-2 darkgrey--text pointer"
+        :class="$route.path == item.path ? 'textcolor--text' : ''"
+      >
+        {{ item.title }}
+      </p>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contentTitle: [],
+    }
+  },
+  async fetch() {
+    this.contentTitle = await this.$content('doc').sortBy('createdAt').fetch()
+  },
+}
+</script>
+
+<style scoped>
+.fixed {
+  position: fixed;
+}
+a {
+  text-decoration: none;
+}
+</style>
