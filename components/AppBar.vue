c1d32feae99b15b41bf5dfd1b06637b101968b62
agfebrian <febriananggi9@gmail.com>
2021-11-16 13:46:46 +0700

    sparkles: menu case study

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <v-app-bar app color="white" flat style="border-bottom: 1px solid #d2d2d2 !important" id="top" height="80px">
+    <v-app-bar
+      app
+      color="white"
+      flat
+      style="z-index: 100; border-bottom: 1px solid #d2d2d2 !important"
+      id="top"
+      height="80px"
+    >
       <v-container class="px-8" fluid>
         <div class="d-flex">
           <div class="logo d-flex align-center pointer" id="logo">
@@ -37,6 +44,73 @@
                 >
                   Product
                 </p>
+                <div class="ml-9 mega-menu-wrapper" @mouseover="showMegaMenu = true" @mouseleave="showMegaMenu = false">
+                  <p class="mb-0 font-weight-bold darkgrey--text pointer">Case Study</p>
+                  <v-scroll-y-transition>
+                    <div v-show="showMegaMenu" class="mega-menu">
+                      <ul>
+                        <li
+                          class="d-flex flex-column text-truncate"
+                          @click="goToLink('https://lp.interconnectdata.com/case-study/case-a/')"
+                        >
+                          <div class="d-flex justify-start align-center pt-1">
+                            <v-img
+                              class="mr-3"
+                              max-width="30px"
+                              :src="require('@/assets/img/icon/startup.svg')"
+                            ></v-img>
+                            <p class="mt-1 mb-0 title">Startup <v-icon small>mdi-arrow-right</v-icon></p>
+                          </div>
+                          <p class="mt-1 text">Get to know your target market easily</p>
+                        </li>
+                        <v-divider></v-divider>
+                        <li
+                          class="d-flex flex-column text-truncate"
+                          @click="goToLink('https://lp.interconnectdata.com/case-study/case-b/')"
+                        >
+                          <div class="d-flex justify-start align-center pt-1">
+                            <v-img
+                              class="mr-3"
+                              max-width="30px"
+                              :src="require('@/assets/img/icon/investors.svg')"
+                            ></v-img>
+                            <p class="mt-1 mb-0 title">Investors <v-icon small>mdi-arrow-right</v-icon></p>
+                          </div>
+                          <p class="mt-1 text">Get valid and relevant information from various prospects</p>
+                        </li>
+                        <v-divider></v-divider>
+                        <li class="d-flex flex-column text-truncate">
+                          <div class="d-flex justify-start align-center pt-1">
+                            <v-img
+                              class="mr-3"
+                              max-width="30px"
+                              :src="require('@/assets/img/icon/market-research.svg')"
+                            ></v-img>
+                            <p class="mt-1 mb-0 title">Market Researcher <v-icon small>mdi-arrow-right</v-icon></p>
+                          </div>
+                          <p class="mt-1 text">Get accurate and reliable information according to your needs</p>
+                        </li>
+                        <v-divider></v-divider>
+                        <li
+                          class="d-flex flex-column text-truncate"
+                          @click="goToLink('https://lp.interconnectdata.com/case-study/case-d/')"
+                        >
+                          <div class="d-flex justify-start align-center pt-1">
+                            <v-img
+                              class="mr-3"
+                              max-width="30px"
+                              max-height="25px"
+                              contain
+                              :src="require('@/assets/img/icon/professional-person.svg')"
+                            ></v-img>
+                            <p class="mt-1 mb-0 title">Professionals <v-icon small>mdi-arrow-right</v-icon></p>
+                          </div>
+                          <p class="mt-1 text">Get your best business opportunities with reliable information</p>
+                        </li>
+                      </ul>
+                    </div>
+                  </v-scroll-y-transition>
+                </div>
                 <p
                   class="mb-0 ml-9 font-weight-bold darkgrey--text pointer"
                   @click="goToLink('https://lp.interconnectdata.com/about')"
@@ -87,7 +161,9 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      showMegaMenu: false,
+    }
   },
   computed: {},
   watch: {},
@@ -102,4 +178,44 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.mega-menu-wrapper {
+  position: relative;
+  height: 80px;
+  display: flex;
+  align-items: center;
+}
+.mega-menu {
+  width: 500px;
+  margin-left: -200px;
+  padding: 10px 20px;
+  border-radius: 10px;
+  position: absolute;
+  top: 70px;
+  background-color: #ffffff;
+  box-shadow: 1px 5px 10px rgba(202, 203, 209, 0.75);
+  -webkit-box-shadow: 1px 5px 10px rgba(202, 203, 209, 0.75);
+  -moz-box-shadow: 1px 5px 10px rgba(202, 203, 209, 0.75);
+  border: 1px solid white !important;
+  // transition: all 0.8s ease-out;
+  ul {
+    margin: 0px;
+    padding: 0px;
+    li {
+      list-style: none;
+      p {
+        color: rgb(56, 52, 52);
+      }
+    }
+    li:hover {
+      cursor: pointer;
+      p {
+        color: black;
+      }
+      i {
+        color: black;
+      }
+    }
+  }
+}
+</style>
